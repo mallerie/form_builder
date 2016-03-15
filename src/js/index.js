0a1f7845a80a4cef4d64547903dec96d8c94327a
Mallerie Shirley <mallerie@gmail.com>
2016-03-15 07:36:16 -0400

    Another Commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,33 @@
 // Javascript Entry Point
 
+import $ from 'jquery';
+
+import users from './form_data';
+
+var $app = $('.app');
+
+
+users.forEach(function(user) {
+  var template;
+  if (user.type === 'text' || user.type === 'email' || user.type === 'tel') {
+    template = text;
+  } else if (user.type === 'select') {
+    template = select;
+  } else if (user.type === 'textarea') {
+    template = textarea;
+  }
+  
+  var html = template(user);
+  $app.append( html );
+});
+
+
+
+
+
+
+
+
+
+
+
